1fd73b5c2a5ccde4b72aec4b5215632c0df69c9b
Wilmer Terrero <wilmerterrero1922@gmail.com>
2024-05-19 16:17:26 -0400

    Initial Commit

---
+++ MacWPM/MacWPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacWPMApp.swift
+//  MacWPM
+//
+//  Created by Wilmer Terrero on 19/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MacWPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
